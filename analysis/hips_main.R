34f31c6e3dd62a904da73f5c86daed82c5a45446
Queena-wang <queena.wang@cliftoninsight.co.uk>
2025-04-15 15:49:38 +0100

    Update hips_main.R

--- analysis/hips_main.R
+++ analysis/hips_main.R
@@ -129,14 +129,14 @@ markov_hips$generate_costs_qalys()
 
 # Summarise the results
 markov_hips$generate_results_table()
-write.csv(markov_hips$generate_results_table(), file = "output/results_table_hips.csv")
+write.csv(markov_hips$generate_results_table(), file = "output/results_table_hips_1.csv")
 
 
 # And export to Excel
 # Need to export model settings (states, cycles, treatments) and
 # sensible names to PSa. Ideally also add formatting to Markov trace and input
 # parameter headings
-markov_hips$export_to_excel(wb_filename = "output/test_output_1_1.xlsm")
+markov_hips$export_to_excel(wb_filename = "output/test_output_hips_1.xlsm")
 
 
 # Check properties of the Markov trace
